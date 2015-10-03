1282c2afed87eedb70b90d9addcb89c68eb62242
Hung Nguyen <hg.ngyn@gmail.com>
2015-10-02 23:48:26 -0700

    Adding template files

--- app.js
+++ app.js
@@ -6,7 +6,10 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
+
+// var http = require('http');
+// var fs = require('fs');
+// var index = fs.readFileSync('views/index.html');
 
 var app = express();
 
@@ -23,7 +26,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
