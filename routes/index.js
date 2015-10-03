e6c108222b51578fc55d33d194b3ac19cb855767
Hung Nguyen <hg.ngyn@gmail.com>
2015-10-03 16:40:37 -0700

    Fixed form submission

--- routes/index.js
+++ routes/index.js
@@ -4,35 +4,19 @@ var request = require('request');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	res.render('index')
-});
-
-router.get('/*', function(req, res, next) {
-	request.get('http://api.kivaws.org/v1/teams/search.json?sort_by=member_count&q=' + req.params[0], function(err, response, body) {
-		if(err) {
-			console.log('invalid')
-			res.render('index');
+	if(req.query['lenderID']) {
+	request.get('http://api.kivaws.org/v1/lenders/' + req.query['lenderID'] + '.json', function(err, response, body) {
+		requestResponse = JSON.parse(body)
+		if(requestResponse['lenders'] == undefined) {
+			res.redirect('/');
 		} else {
 			console.log(body)
-			res.render('results', {lenderID: req.params[0]});
+			res.render('results', {lenderID: req.query['lenderID']});
 		}
 	})
+	} else {
+		res.render('index')
+	}
 });
-
-// router.get('/', function(req, res, next) {
-// 	if(req.query['lenderID']) {
-// 		var lender = req.query['lenderID']
-// 		request	
-// 			.get('http://api.kivaws.org/v1/lenders/' + lender + '.json', function(err, res, body) {
-// 				if(!err && res.statusCode == 200) {
-// 					console.log(body)
-// 				} 
-// 			})  
-// 			.on('error', function(err) {
-//     			res.redirect('/')
-//   			})
-// 		res.render('results', {lenderID: lender});
-// 	}
-// });
 
 module.exports = router;
